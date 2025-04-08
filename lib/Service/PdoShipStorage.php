143e8e05609879b44ec047cffe3f585c3953a1f5
Dmitry Vasilyuk <vasilukwolf@gmail.com>
2025-04-08 18:50:14 +0400

    Different Exception Classes

--- lib/Service/PdoShipStorage.php
+++ lib/Service/PdoShipStorage.php
@@ -13,7 +13,7 @@ class PdoShipStorage implements ShipStorageInterface
 
     public function fetchAllShipsData()
     {
-        $statement = $this->pdo->prepare('SELECT * FROM FOOOO');
+        $statement = $this->pdo->prepare('SELECT * FROM ship');
         $statement->execute();
 
         return $statement->fetchAll(\PDO::FETCH_ASSOC);
@@ -21,6 +21,7 @@ class PdoShipStorage implements ShipStorageInterface
 
     public function fetchSingleShipData($id)
     {
+        throw new \InvalidArgumentException('somthing else went wrong');
         $statement = $this->pdo->prepare('SELECT * FROM ship WHERE id = :id');
         $statement->execute(array('id' => $id));
         $shipArray = $statement->fetch(\PDO::FETCH_ASSOC);
