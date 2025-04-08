55cd7dd707569623826a8f7a3f4f12141e6416cc
Dmitry Vasilyuk <vasilukwolf@gmail.com>
2025-04-08 20:19:16 +0400

    Object Composition FTW!

--- lib/Service/Container.php
+++ lib/Service/Container.php
@@ -54,6 +54,7 @@ class Container
         if ($this->shipStorage === null) {
             $this->shipStorage = new PdoShipStorage($this->getPDO());
             //w$this->shipStorage = new JsonFileShipStorage(__DIR__.'/../../resources/ships.json');
+            $this->shipStorage = new LoggableShipStorage($this->shipStorage);
         }
 
         return $this->shipStorage;
---
+++ lib/Service/LoggableShipStorage.php
@@ -0,0 +1,30 @@
+<?php
+namespace Service;
+
+class LoggableShipStorage implements ShipStorageInterface
+{
+    private $shipStorage;
+    public function __construct(ShipStorageInterface $shipStorage)
+    {
+        $this->shipStorage = $shipStorage;
+    }
+    
+    private function log($message)
+    {
+        // todo - actually log this somewhere, instead of printing!
+        echo $message;
+    }
+
+    public function fetchAllShipsData()
+    {
+        $ships =  $this->shipStorage->fetchAllShipsData();
+
+        $this->log(sprintf('Just fetched %s ships', count($ships)));
+        
+        return $ships;
+    }
+    public function fetchSingleShipData($id)
+    {
+        return $this->shipStorage->fetchSingleShipData($id);
+    }
+}
