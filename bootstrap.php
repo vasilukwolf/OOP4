acf4e72df71de05d83ad1b0e781d80ce89bc8f3e
Dmitry Vasilyuk <vasilukwolf@gmail.com>
2025-04-08 17:01:31 +0400

    Composer Autoloading

--- bootstrap.php
+++ bootstrap.php
@@ -1,12 +1,6 @@
 <?php
 
-spl_autoload_register(function($className) {
-    $path = __DIR__.'/lib/'.str_replace('\\', '/', $className).'.php';
-    if (file_exists($path)) {
-        require $path;
-    }
-    // we don't support this class!
-});
+require __DIR__.'/vendor/autoload.php';
 
 $configuration = array(
     'db_dsn'  => 'mysql:host=127.127.126.50;dbname=oo_battle',
